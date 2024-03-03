d75a260c515aafe9df87f15753072ae64f8c6503
mnofal225 <mnofal225@gmail.com>
2024-03-03 08:55:50 -0600

    second commit

---
+++ src/test/java/com/mobile/companion/CompanionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobile.companion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
